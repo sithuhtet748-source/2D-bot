bb917ad06df79c75f8ccd8845ceb21638b647592
sithuhtet748-source <sithuhtet748@gmail.com>
2026-09-10 00:37:39 +0630

    Fix newline at end of file in main.py

--- main.py
+++ main.py
@@ -65,4 +65,3 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
-      
